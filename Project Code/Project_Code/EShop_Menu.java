ec8807df797cc1039fd1169a466f2b09172fddff
dimitrispap1072583 <128745981+dimitrispap1072583@users.noreply.github.com>
2023-06-10 16:54:12 +0300

    Add files via upload

--- Project Code/Project_Code/EShop_Menu.java
+++ Project Code/Project_Code/EShop_Menu.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class EShop_Menu {
-    public static ArrayList<Order> orderList = new ArrayList<>();
-   static void eshopStart(){
+   public static ArrayList<Order> orderList = new ArrayList<Order>();
+   static void eshopStart(User_profile u){
         System.out.println("1.Browse Shop, 2.Order Tracking");
         Scanner input = new Scanner(System.in);
         int choice2 = input.nextInt();
         switch (choice2){ 
-            case 1: EShop_Menu.openEShop(); break;
+            case 1: EShop_Menu.openEShop(u); break;
             case 2: EShop_Menu.openTracker(); break;
             default: break;
         }
    }
 
     
-   static void openEShop() {
+   static void openEShop(User_profile u) {
         Shop shop = new Shop();//apo to menou tou eshop pame sto shop kommati tou
-        shop.browseShop();
+        shop.browseShop(u);
    }
 
    static void openTracker(){
